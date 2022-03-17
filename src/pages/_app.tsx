879ad1733de853c74b91dad665c81bac0ad765b3
Alice Maria <alice.lima@imd.ufrn.br>
2022-03-17 18:00:03 -0300

    Configuração styled e criação de estilos globais

--- src/pages/_app.tsx
+++ src/pages/_app.tsx
@@ -1,8 +1,19 @@
 import '../styles/globals.css'
 import type { AppProps } from 'next/app'
+import Head from 'next/head'
+
+import GlobalStyle from 'styles/global'
 
 function MyApp({ Component, pageProps }: AppProps) {
-  return <Component {...pageProps} />
+  return (
+    <>
+      <Head>
+        <title>E-commerce em Next</title>
+      </Head>
+      <GlobalStyle />
+      <Component {...pageProps} />
+    </>
+  )
 }
 
 export default MyApp
